94a5047a8e5e03ba04546a3b73e97230ece66bb0
moonlin <moonlin.coder@gmail.com>
2022-10-16 18:22:31 +0300

    unknown command case

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from modules import bases
 
+
 def safe_input(text, min, max):
     while True:
         num = int(input(text))
         if (min <= num) and (num <= max):
             return num
 
+
 if __name__ == '__main__':
     base = bases[-1][1]
-    print(f"Выбрана последняя база: \"{base[0]}\", для смены наберите cd ")
+    print(f"Выбрана последняя база: \"{bases[-1][0]}\", для смены наберите cd ")
     while True:
         a = input(" >_  ")
         print()
@@ -21,20 +23,29 @@ if __name__ == '__main__':
             a = a.split()
             if a[0] == 'h':
                 if len(a) == 1:
-                    print('Доступные команды:'
-                          'h <n> - подробное описание модуля или эта подсказка'
-                          'ls - вывести список модулей текущей базы'
-                          'ld - вывести список баз'
-                          'cd - сменить базу'
+                    print('Доступные команды: \n'
+                          ' h <n> - подробное описание модуля или эта подсказка \n'
+                          ' ls - вывести список модулей текущей базы \n'
+                          ' ld - вывести список баз \n'
+                          ' cd - сменить базу \n'
                           '')
                 else:
                     for module in a[1:]:
                         print(base[int(module)].info)
-            if a[0] == 'ls':
+            elif a[0] == 'ls':
                 for i, module in enumerate(base):
                     print(f"{i}: {module.name}")
-            if a[0] == 'ld':
+            elif a[0] == 'ld':
                 for i, base in enumerate(bases):
                     print(f"{i}: {base[0]}")
-            if a[0] == 'cd':
-                base = bases[safe_input(" база > ", 0, len(bases) - 1)][1]
+            elif a[0] == 'cd':
+                if len(a) == 2:
+                    index = int(a[1])
+                    if 0 <= index <= (len(bases) - 1):
+                        base = bases[index]
+                else:
+                    base = bases[safe_input(" база > ", 0, len(bases) - 1)]
+                print(f"Текущая база: {base[0]}")
+                base = base[1]
+            else:
+                print("Неизвестная команда, напишите h")
