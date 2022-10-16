ea327a395ce305114557e2bffe2dbe62fd79f478
moonlin <moonlin.coder@gmail.com>
2022-10-16 18:14:01 +0300

    Prettified menu

--- main.py
+++ main.py
@@ -7,18 +7,34 @@ def safe_input(text, min, max):
             return num
 
 if __name__ == '__main__':
-    print("Выберите базу: ")
-    for i, base in enumerate(bases):
-        print(f"{i}: {base[0]}")
-
-    base = bases[safe_input(" база > ", 0, len(bases) - 1)][1]
-
+    base = bases[-1][1]
+    print(f"Выбрана последняя база: \"{base[0]}\", для смены наберите cd ")
     while True:
-        a = input(" номер задания >> ")
+        a = input(" >_  ")
         print()
-        if a == 'h':
-            a
-        elif a.isdecimal():
+
+        if a.isdecimal():
             a = int(a)
             if (0 <= a) and (a <= len(base)):
                 base[a]().run()
+        else:
+            a = a.split()
+            if a[0] == 'h':
+                if len(a) == 1:
+                    print('Доступные команды:'
+                          'h <n> - подробное описание модуля или эта подсказка'
+                          'ls - вывести список модулей текущей базы'
+                          'ld - вывести список баз'
+                          'cd - сменить базу'
+                          '')
+                else:
+                    for module in a[1:]:
+                        print(base[int(module)].info)
+            if a[0] == 'ls':
+                for i, module in enumerate(base):
+                    print(f"{i}: {module.name}")
+            if a[0] == 'ld':
+                for i, base in enumerate(bases):
+                    print(f"{i}: {base[0]}")
+            if a[0] == 'cd':
+                base = bases[safe_input(" база > ", 0, len(bases) - 1)][1]
